e3c379cc5cdcba4e8d58f66d6a4212f02bc56746
cprogramer <perica.klipeta@gmail.com>
2017-10-17 10:33:00 +0200

    sredjivanje stila

--- hello.c
+++ hello.c
@@ -8,14 +8,15 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Petar Misic");
 MODULE_DESCRIPTION("My first module :)");
 
-static __init int hello_init(void) {
+static __init int hello_init(void)
+{
 	pr_debug("Hello world!\n");
 	return 0;
 }
 
-static void hello_exit(void) {
+static void hello_exit(void)
+{
 	pr_debug("Goodbye world!\n");
-	return;
 }
 
 module_init(hello_init);
