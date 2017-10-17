e1ba6b821fdd489da0012a7cd06af3f772411d20
cprogramer <perica.klipeta@gmail.com>
2017-10-17 10:49:53 +0200

    jiffies ftw

--- hello.c
+++ hello.c
@@ -3,6 +3,11 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/types.h>
+#include <linux/jiffies.h>
+
+u64 start;
+u64 end;
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Petar Misic");
@@ -10,13 +15,19 @@ MODULE_DESCRIPTION("My first module :)");
 
 static __init int hello_init(void)
 {
+	start = get_jiffies_64();
 	pr_debug("Hello world!\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
+	end = get_jiffies_64();
+	struct timeval time;
+
+	jiffies_to_timeval(end - start, &time);
 	pr_debug("Goodbye world!\n");
+	pr_debug("We were loaded for %ld seconds!\n", time.tv_sec);
 }
 
 module_init(hello_init);
